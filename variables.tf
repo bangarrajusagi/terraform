e721f3ffd835f7946e01c45fc47eeb45338c4a81
vsmanivarma <94699976+vsmanivarma@users.noreply.github.com>
2022-11-15 18:45:10 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-    description = "Name of the Resource Group"
-    type = string
-}
-
-variable "location" {
-    description = "Location of the Resource group"
-    type = string
-}
